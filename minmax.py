6e69aaf46fe449635c66d3780cc5323a1944bf3c
abhibembalagi <51333444+abhibembalagi@users.noreply.github.com>
2019-07-27 19:18:39 +0530

    Update minmax.py

--- minmax.py
+++ minmax.py
@@ -1,4 +1,5 @@
 #!/bin/python
+#!/bin/python
 
 import math
 import os
@@ -8,12 +9,14 @@ import sys
 
 # Complete the miniMaxSum function below.
 def miniMaxSum(arr):
-    length=len(arr)
-    sum=0
-    for i in range(length):
-        sum=sum+arr[i]
-    print( sum-max(arr),sum-min(arr))
-if __name__ == '__main__':
-    arr = map(int, raw_input().rstrip().split())
-
-    miniMaxSum(arr)
+    min_ele=sys.maxint
+    max_ele=0
+    arr_sum=0
+    for val in arr:
+        min_ele=min(min_ele,val)
+        max_ele=max(max_ele,val)
+        arr_sum+=val
+    return arr_sum-max_ele,arr_sum-min_ele
+if __name__=='__main__':
+    arr=map(int,raw_input().rstrip().split())
+    print" ".join(map(str,miniMaxSum(arr)))
