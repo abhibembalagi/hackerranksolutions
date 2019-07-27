048eb97214f48418b646d15879dc66a33d080202
abhibembalagi <51333444+abhibembalagi@users.noreply.github.com>
2019-07-27 20:23:59 +0530

    Create gemstones.py

---
+++ gemstones.py
@@ -0,0 +1,8 @@
+n = int(raw_input())
+first_input = set(raw_input())
+
+for i in range(n-1):
+    rest_input = set(raw_input())
+    first_input = first_input & rest_input
+
+print(len(first_input))
